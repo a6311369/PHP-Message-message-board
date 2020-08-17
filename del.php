a7e99318a752bfbd0cb650d5b124943c69f4fe4b
tuffy_lin <tuffy_lin@mail.chungyo.net>
2020-08-17 18:20:14 +0800

    查詢留言可以回覆留言Final

--- del.php
+++ del.php
@@ -7,4 +7,10 @@ $tis = $conn->prepare($query);
 $tis->bindParam(1, $del_id);
 $tis->execute();
 
+$del_id2 = $_POST['del_id'];
+$query2 = "DELETE FROM reply WHERE `id` = ?";
+$tis2 = $conn->prepare($query2);
+$tis2->bindParam(1, $del_id);
+$tis2->execute();
+
 require_once "list.php";
