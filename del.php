331d806f507e0567a557cd000c2619259a3b8edb
tuffy_lin <tuffy_lin@mail.chungyo.net>
2020-08-18 14:58:42 +0800

    del.php modify

--- del.php
+++ del.php
@@ -2,15 +2,10 @@
 require_once "pdo.php";
 
 $del_id = $_POST['del_id'];
-$query = "DELETE FROM msg WHERE `id` = ?";
+$query = "DELETE a.*, b.* FROM msg as a, reply as b WHERE a.id = b.msg_id AND a.id = ?";
 $tis = $conn->prepare($query);
 $tis->bindParam(1, $del_id);
 $tis->execute();
 
-$del_id2 = $_POST['del_id'];
-$query2 = "DELETE FROM reply WHERE `id` = ?";
-$tis2 = $conn->prepare($query2);
-$tis2->bindParam(1, $del_id);
-$tis2->execute();
 
 require_once "list.php";
