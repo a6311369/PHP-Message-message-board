f9b0f80fa6a80de86321666112b02312c8392301
tuffy_lin <tuffy_lin@mail.chungyo.net>
2020-08-18 11:44:54 +0800

    replay table schema modify

--- message.sql
+++ message.sql
@@ -1,7 +1,7 @@
 /*
  Navicat Premium Data Transfer
 
- Source Server         : LAB-DB
+ Source Server         : Lab
  Source Server Type    : MySQL
  Source Server Version : 50731
  Source Host           : 192.168.132.29:3306
@@ -11,7 +11,7 @@
  Target Server Version : 50731
  File Encoding         : 65001
 
- Date: 14/08/2020 13:48:12
+ Date: 18/08/2020 11:43:48
 */
 
 SET NAMES utf8mb4;
@@ -24,8 +24,19 @@ DROP TABLE IF EXISTS `msg`;
 CREATE TABLE `msg`  (
   `name` varchar(50) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
   `descr` varchar(200) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
-  `id` int(5) NOT NULL,
+  `id` int(6) NOT NULL AUTO_INCREMENT,
   PRIMARY KEY (`id`) USING BTREE
-) ENGINE = InnoDB CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Dynamic;
+) ENGINE = InnoDB AUTO_INCREMENT = 13 CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- Table structure for reply
+-- ----------------------------
+DROP TABLE IF EXISTS `reply`;
+CREATE TABLE `reply`  (
+  `msg_id` int(6) NOT NULL,
+  `message` varchar(200) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
+  `id` int(6) NOT NULL AUTO_INCREMENT,
+  PRIMARY KEY (`id`) USING BTREE
+) ENGINE = InnoDB AUTO_INCREMENT = 19 CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Dynamic;
 
 SET FOREIGN_KEY_CHECKS = 1;
