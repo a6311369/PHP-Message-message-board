97d17a6821e5ef1f3183bd3ebd3a718aa5d9f7cc
root <root@localhost.localdomain>
2020-08-29 18:41:22 +0800

    remove reply final

--- doctrine/remove_reply.php
+++ doctrine/remove_reply.php
@@ -2,9 +2,20 @@
 require_once "bootstrap.php";
 
 $msgId = trim($_POST['del_re_id']);
-$msg = $entityManager->find('Reply', $msgId);
-$entityManager->remove($msg);
+$message = trim($_POST['del_re_message']);
+
+$msgRepository = $entityManager->getRepository('Msg');
+$msg = $msgRepository->findAll();
+
+$replyId = $msgId;
+$replyRepository = $entityManager->getRepository('Reply');
+$reply = $replyRepository->findBy(array('id' => $replyId, 'message' => $message));
+
+foreach ($reply as $reply) {
+    // echo 'test' . $reply->getMessage() . '<br>';
+
+    $entityManager->remove($reply);
+}
+// }
 $entityManager->flush();
-
-
 require_once "list_msg.php";
