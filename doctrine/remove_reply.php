0aeeb7addbad746c12e04013600342ac9b986462
tuffy_lin <tuffy_lin@mail.chungyo.net>
2020-08-31 10:28:05 +0800

    remove reply final and add annotation

--- doctrine/remove_reply.php
+++ doctrine/remove_reply.php
@@ -4,18 +4,17 @@ require_once "bootstrap.php";
 $msgId = trim($_POST['del_re_id']);
 $message = trim($_POST['del_re_message']);
 
+//先找出關聯的主表，Msg Entity，不先找出來這個附表會不知道關聯的，因為有設定Entity associations
 $msgRepository = $entityManager->getRepository('Msg');
 $msg = $msgRepository->findAll();
 
-$replyId = $msgId;
+//再去找關聯的附表要去刪除的表，Reply Entity 
 $replyRepository = $entityManager->getRepository('Reply');
-$reply = $replyRepository->findBy(array('id' => $replyId, 'message' => $message));
+$reply = $replyRepository->findBy(array('id' => $msgId, 'message' => $message));
 
 foreach ($reply as $reply) {
-    // echo 'test' . $reply->getMessage() . '<br>';
-
     $entityManager->remove($reply);
 }
-// }
 $entityManager->flush();
+
 require_once "list_msg.php";
