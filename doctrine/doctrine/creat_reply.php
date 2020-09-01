53701ce9693221333e87ca30996d3e9936808ac6
tuffy_lin <tuffy_lin@mail.chungyo.net>
2020-09-01 17:10:52 +0800

    利用getRepository的方式做留言總數final

---
+++ doctrine/doctrine/creat_reply.php
@@ -0,0 +1,16 @@
+<?php
+require_once "bootstrap.php";
+
+$newMsg = trim($_POST['reply_id']);
+$newMessage = trim($_POST['reply_message']);
+
+$msg = $entityManager->find('Msg', $newMsg);
+
+$newReply = new Reply();
+$newReply->setMsg($msg);
+$newReply->setMessage($newMessage);
+
+$entityManager->persist($newReply);
+$entityManager->flush();
+
+require_once "list_msg.php";
