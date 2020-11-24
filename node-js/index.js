2f86143dd87605d1cdd8a72733dd1f81b45dab55
tuffy_lin <tuffy_lin@mail.chungyo.net>
2020-11-24 11:08:11 +0800

    add nodejs

---
+++ node-js/index.js
@@ -0,0 +1,19 @@
+const express = require('express')
+const app = express()
+const port = 8888
+const redis = require('redis');
+const client = redis.createClient(); // this creates a new client
+
+app.get('/creat/:key/value/:value', function(req, res) {
+client.set(req.params.key, req.params.value, redis.print);});
+
+app.get('/read/:key', function(req, res) {
+client.get(req.params.key, redis.print);});
+
+app.get('/del/:key', function(req, res) {
+client.del(req.params.key, redis.print);});
+
+
+app.listen(port, function(){
+  console.log(`Example listening at http://192.168.56.101:${port}`)
+)
